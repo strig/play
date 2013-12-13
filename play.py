3fa148dfad0e69bde4f2ff90e0edce8c6c1d88a5
Neal Striegler-Pettersson <strigs@gmail.com>
2013-12-12 22:40:31 -0500

    add the powershell alias

--- play.py
+++ play.py
@@ -1,37 +1,38 @@
 import os
 import subprocess
 import random
-from sys import argv
+import argparse
 
+## Add the following to the powershell profile
+# function play($show, $count)
+# {
+# 	python C:\directory\play.py $show $count
+# }
 
 futurama_path = 'D:\\video\\Futurama (complete)\\'
 tng_path = 'D:\\trek\\The Next Generation\\'
 vlc_path = "C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe"
 
+parser = argparse.ArgumentParser()
 
-if len(argv) < 2:
-	episodes_to_play = 1
+parser.add_argument("show", help=" The show you want to play. 'tng' or 'futu' ")
+parser.add_argument("play_count", type=int, help="number of episodes you want to play")
+
+args = parser.parse_args()
+
+if args.show.lower() == 'tng':
+	show_path = tng_path
+elif args.show.lower() == 'futu':
 	show_path = futurama_path
-elif len(argv) < 3:
-	episodes_to_play = 1
-	if str(argv[1]).lower() == 'tng':
-		show_path = tng_path
-	else:
-		show_path = futurama_path
 else:
-	if str(argv[1]).lower() == 'tng':
-		show_path = tng_path
-	else:
-		show_path = futurama_path
-	episodes_to_play = int(argv[2])
-
+	print "pick either tng or futurama"
 
 episodes = []
 
 def play_episode(video):
 	subprocess.call([vlc_path, video, '--play-and-exit', '--fullscreen'])
 
-for i in range(episodes_to_play):
+for i in range(args.play_count):
 	# randomly select a list of episodes to play
 	season =  random.choice(os.listdir(show_path))
 	while season == "Futurama Movies":
